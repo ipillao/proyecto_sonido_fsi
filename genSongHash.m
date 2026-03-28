052775d077ab58884ae0701eb399acf8eedd8fac
Iván <pillado.rodriguez.ivan@gmail.com>
2026-03-28 18:43:32 +0100

    Se ha creado la función analizadorPicos para mejorar la modularidad del código. Todas las funciones que necesiten sacar los hashes de cualquier audio llaman a esta función. Así podemos depurar mejor el código.
    Funciona como era de esperar. Falta implementar la generación de hash robusta a diferentes hilos y la mejor detección de la canción con ruido.

--- genSongHash.m
+++ genSongHash.m
@@ -1,5 +1,5 @@
 function genSongHash(fileSong,database)
-%genSongHas
+%genSongHash
 %   Guarda los hashes de las relaciones de la canción que se le pase por
 %   parámetro en la colección clave-valor que se le pase por parámetro.
 
@@ -13,103 +13,15 @@ id_cancion = codigo_cancion;
 
 [audio, fs] = audioread(fileSong); %"./Songs/003_mono.mp3"
 
-%spectrogram(audio, hann(8192), 4096, 8192, fs, 'yaxis');
-
-
-%S: matriz que contiene los niveles de energía en cada ventana, 
-% W: matriz que contiene las frecuencias que corresponden en la tabla S 
-% T: matriz que contiene los tiempos en los que suceden las ventanas de la
-
-[S, F, T] = spectrogram(audio, hann(2048), 1024, 2048, fs, 'yaxis');
-
-% Matriz S es la matriz de amplitud compleja del espectrograma. S son números complejos
-
-S_mag = abs(S); % Pasamos S a magnitud, haciendo el módulo.
-
-S_log = 20*log10(S_mag); %+eps
-
-nivel_truncado = max(S_log(:)) - 40; % Calcula el nivel mínimo de amplitud para truncar
-
-S_truncado = S_log;
-
-S_truncado(S_truncado < nivel_truncado) = -Inf;
-
-size_celda = 30; % Tamaño de la celda con que se se compara para sacar picos máximos
-
-picos_f = []; % Inicializar el vector para almacenar índices de frecuencia
-picos_t = []; % Inicializar el vector para almacenar índices de tiempo
-
-for i = 1:size_celda:size(S_log,1)
-    for j = 1:size_celda:size(S_log,2)
-
-        f_fin = min(i+size_celda-1, size(S_log,1));
-        t_fin = min(j+size_celda-1, size(S_log, 2));
-
-        bloque = S_truncado(i:f_fin, j:t_fin);
-
-        [~, idx] = max(bloque(:));
-
-        if all(isinf(bloque(:)))
-            continue
-        end
-
-        %idx es el índice LINEAL del máximo del bloque 
-
-        [bi, bj] = ind2sub(size(bloque), idx);
-
-        %bi y bj son los índices x y del máximo en ese bloque. Hay que
-        %pasarlos a coordenadas globales de S
-
-        %Pasar a coordenadas globales
-
-        picos_f(end+1) = i + bi - 1;
-        picos_t(end+1) = j + bj - 1;
-
-    end
-end
-
-% Crear las relaciones entre los picos picos
-
-delta_t_min = 0.1; % segundos
-delta_t_max = 1.0; % segundos
-
 if isfile(database)
     load(database, "db");
 else
     db = dictionary();
 end
 
-for k = 1:length(picos_f)
+claves = analizadorPicos(audio,fs);
 
-    f_anchor = picos_f(k);
-    t_anchor = picos_t(k);
-
-    % Buscar picos en la zona objetivo
-
-    for m = k+1:length(picos_f)
-
-        f_target = picos_f(m);
-        t_target = picos_t(m);
-
-        dt = T(t_target) - T(t_anchor);
-
-        if dt > delta_t_min && dt < delta_t_max
-            % Crear hash
-            clave = keyHash([round(F(f_anchor)), round(F(f_target)), round(dt,2)]);
-
-            valor = id_cancion;
-
-            % if isKey(db, clave)
-            %     db(clave) = [db(clave); valor];
-            % else
-            db(clave) = valor;
-            
-            %end
-            %disp(clave);
-
-        end
-    end
-end
+db(claves)=id_cancion;
 
 save(database, "db");
 disp("Guardado correctamente: "+id_cancion);
--- matchSongNoRandom.m
+++ matchSongNoRandom.m
@@ -17,61 +17,6 @@ inicio = fs*inicio;
 
 fragmento = audio(inicio: inicio+(Ftime*fs)-1,1);
 
-% Analizar el fragmento
-
-[S, F, T] = spectrogram(fragmento, hann(2048), 1024, 2048, fs, 'yaxis');
-
-S_mag = abs(S); % Pasamos S a magnitud, haciendo el módulo.
-
-S_log = 20*log10(S_mag);
-
-nivel_truncado = max(S_log(:)) - 40; % Calcula el nivel mínimo de amplitud para truncar
-
-S_truncado = S_log;
-
-S_truncado(S_truncado < nivel_truncado) = -Inf;
-
-size_celda = 30; % Tamaño de la celda con que se se compara para sacar picos máximos
-
-picos_f = []; % Inicializar el vector para almacenar índices de frecuencia
-picos_t = []; % Inicializar el vector para almacenar índices de tiempo
-
-for i = 1:size_celda:size(S_log,1)
-    for j = 1:size_celda:size(S_log,2)
-
-        f_fin = min(i+size_celda-1, size(S_log,1));
-        t_fin = min(j+size_celda-1, size(S_log, 2));
-
-        bloque = S_truncado(i:f_fin, j:t_fin);
-
-        [~, idx] = max(bloque(:));
-
-        if all(isinf(bloque(:)))
-            continue
-        end
-
-        %idx es el índice LINEAL del máximo del bloque 
-
-        [bi, bj] = ind2sub(size(bloque), idx);
-
-        %bi y bj son los índices x y del máximo en ese bloque. Hay que
-        %pasarlos a coordenadas globales de S
-
-        %Pasar a coordenadas globales
-
-        picos_f(end+1) = i + bi - 1;
-        picos_t(end+1) = j + bj - 1;
-
-    end
-end
-
-% Crear las relaciones entre los picos picos
-
-delta_t_min = 0.1; % segundos
-delta_t_max = 1.0; % segundos
-
-coincidencias = [];
-
 if isfile("database.mat")
     load(database, "db");
 else
@@ -79,42 +24,11 @@ else
     return
 end
 
-for k = 1:length(picos_f)
+claves = analizadorPicos(fragmento,fs); % Analizar el fragmento
 
-    f_anchor = picos_f(k);
-    t_anchor = picos_t(k);
-    f1 = floor(F(f_anchor)/30)*30;
-    % Buscar picos en la zona objetivo
-
-    for m = k+1:length(picos_f)
-
-        f_target = picos_f(m);
-        t_target = picos_t(m);
-        f2 = floor(F(f_target)/30)*30;
-        dt = T(t_target) - T(t_anchor);
-        dt_q = floor(dt*10)/10;
-        if dt_q > delta_t_min && dt < delta_t_max
-            % Crear hash
-            clave = keyHash([f1, f2, dt_q]);
-
-            
-            if isKey(db, clave)
-                coincidencias = [coincidencias,db(clave)]; % Se guardan los id de canciones que coinciden con los hashes generados
-
-                %t_anchor_match = T(t_anchor); 
-                
-                % for i = 1:size(apariciones,1)
-                %     song_id_db = apariciones(i,1);
-                %     t_anchor_db = apariciones(i,2);
-                %     %coincidencias = [coincidencias; song_id_db, t_anchor_db];
-
-                % end
-
-            end
-        end
-    end
-end
-
+mascara = isKey(db,claves);
+claves = claves(mascara);
+coincidencias = db(claves);
 % Votar por la canción que más se matchea
 
 if isempty(coincidencias)
